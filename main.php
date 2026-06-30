c28832e60446742cc550af9d8315aab86a456a1e
Ferran <ferran.docencia@gmail.com>
2026-06-30 09:43:35 +0200

    2026-06-29

--- main.php
+++ main.php
@@ -9,10 +9,20 @@ require_once
 
 require_once 'conexion.php';
 
+// Definir la query
+$sql = 'SELECT u.nombre_user, c.nombre_color FROM users u JOIN colores c USING(id_color);';
+
+// Preparar la respuesta
+$respuesta = $pdo->prepare($sql);
+
+// Realizar la petición
+$respuesta-> execute();
+
+
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -26,6 +36,8 @@ require_once 'conexion.php';
     <main>
         <section>
             <h2>Usuarios</h2>
+
+
         </section>
                 <section>
             <h2>Dinos tu color preferido</h2>
