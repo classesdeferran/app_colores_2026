e61d3fbc9081c558cad6061b1e4495f7683b5517
Ferran <ferran.docencia@gmail.com>
2026-06-30 19:53:54 +0200

    2026-06-30

--- insert.php
+++ insert.php
@@ -15,7 +15,7 @@ $nombre_usuario = trim($_POST['nombre_i']);
 
 $patron = '/^[ \p{L}0-9]+$/u';
 
-if (empty($nombre_usuario) || !preg_match($patron, $nombre_usuario) ) {
+if (empty($nombre_usuario) || !preg_match($patron, $nombre_usuario)) {
     $_SESSION['error_insert'] = "true";
     $_SESSION['nombre_invalido'] = $nombre_usuario;
     // echo 'error';
@@ -28,21 +28,65 @@ $select_usuario = 'SELECT id_user FROM users WHERE nombre_user = :nombre_usuario
 // 2. Preparar la respuesta
 $respuesta = $pdo->prepare($select_usuario);
 // 3. Realizar la petición
-$respuesta-> execute([
+$respuesta->execute([
     ':nombre_usuario' => $nombre_usuario
 ]);
 // 4. Recuperar los datos de la consulta
 $resultado = $respuesta->fetch();
 
 // Si el usuario ya existe NO hacemos el insert
-if($resultado) {
+if ($resultado) {
     $_SESSION['nombre_existe'] = true;
     header('Location: main.php');
     exit();
 }
 
+// 1. Definir la query
+$select_color = 'SELECT id_color FROM colores WHERE nombre_color = :color';
+// 2. Preparar la respuesta
+$respuesta = $pdo->prepare($select_color);
+// 3. Realizar la petición
+$respuesta->execute([
+    ':color' => $_POST['color_i']
+]);
+// 4. Recuperar los datos de la consulta
+$resultado = $respuesta->fetch();
 
+$id_color = '';
 
+if ($resultado) {
+    // si el color ya está en la BD
+    $id_color = $resultado['id_color'];
+} else {
 
-// $insert_user = 'INSERT INTO users(nombre_user) VALUES'
+    try {
+        // Si no está los insertamos
+        // 1. Definir la query
+        $insert_color = 'INSERT INTO colores(nombre_color) VALUES(:color);';
+        // 2. Preparar la respuesta
+        $respuesta = $pdo->prepare($insert_color);
+        // 3. Realizar la petición
+        $respuesta->execute([
+            ':color' => $_POST['color_i']
+        ]);
+        // El último id insertado en el objero pdo
+        $id_color = $pdo->lastInsertId();
+    } catch (PDOException $err) {
+        echo "Error -> " . $err->getMessage();
+    }
+}
 
+try {
+    // Ya se puede hacer el insert del usuario
+    $insert_user = 'INSERT INTO users(nombre_user, id_color) VALUES(:nombre_user, :id_color)';
+    $insert = $pdo->prepare($insert_user);
+    $insert->execute([
+        'id_color' => $id_color,
+        ':nombre_user' => $nombre_usuario
+    ]);
+} catch (PDOException $err) {
+    echo "Error -> " . $err->getMessage();
+}
+
+session_destroy();
+header('Location: main.php');
--- main.php
+++ main.php
@@ -21,7 +21,6 @@ require_once
 
 require_once 'conexion.php';
 
-<<<<<<< HEAD
 // 1. Definir la query
 $sql = 'SELECT u.nombre_user, c.nombre_color FROM users u JOIN colores c USING(id_color);';
 
@@ -29,7 +28,7 @@ $sql = 'SELECT u.nombre_user, c.nombre_color FROM users u JOIN colores c USING(i
 $respuesta = $pdo->prepare($sql);
 
 // 3. Realizar la petición
-$respuesta-> execute();
+$respuesta->execute();
 
 // 4. Recuperar los datos de la consulta
 $resultado = $respuesta->fetchAll();
@@ -39,28 +38,26 @@ $resultado = $respuesta->fetchAll();
 
 unset($_SESSION['error_insert']);
 unset($_SESSION['nombre_invalido']);
-=======
-// Definir la query
-$sql = 'SELECT u.nombre_user, c.nombre_color FROM users u JOIN colores c USING(id_color);';
+unset($_SESSION['nombre_existe']);
 
-// Preparar la respuesta
-$respuesta = $pdo->prepare($sql);
 
-// Realizar la petición
-$respuesta-> execute();
-
->>>>>>> c28832e60446742cc550af9d8315aab86a456a1e
 
 ?>
 
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Colores</title>
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
+
+    <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script> -->
     <link rel="stylesheet" href="css/style.css">
 </head>
+
 <body>
 
 
@@ -70,48 +67,91 @@ $respuesta-> execute();
     <main>
         <section>
             <h2>Usuarios</h2>
-<<<<<<< HEAD
-            <?php foreach($resultado as $usuario) : ?>
+
+            <?php foreach ($resultado as $usuario) : ?>
                 <div class="user" style="background-color: <?= $usuario['nombre_color'] ?>" ;>
-                    <p>Usuario: <?= $usuario['nombre_user'] ?>, color: <?= $usuario['nombre_color'] ?></p>
+                    <div>
+                        <p class="color_adaptado">Usuario: <?= $usuario['nombre_user'] ?>, color: <?= substr($usuario['nombre_color'], 1)   ?></p>
+                    </div>
+                    <div>
+                        <a href="main.php?nombre=<?= $usuario['nombre_user'] ?>&color=<?= substr($usuario['nombre_color'], 1)   ?>">
+                            <i class="bi bi-pencil-fill color_adaptado"></i>
+                        </a>
+                        <a href=""><i class="bi bi-person-x-fill color_adaptado"></i></a>
+
+
+                    </div>
                 </div>
 
             <?php endforeach; ?>
-=======
-
->>>>>>> c28832e60446742cc550af9d8315aab86a456a1e
 
         </section>
-                <section>
-            <h2>Dinos tu color preferido</h2>
-            <form action="insert.php" method="post">
-                <div class="div_insert">
-                    <div>
-                        <label for="nombre_i">Nombre:</label>
-                        <input type="text" id='nombre_i' name='nombre_i' 
-                        value = <?= $nombre_invalido ?>>
+        <?php if (!$_GET) : ?>
+            <section>
+                <h2>Dinos tu color preferido</h2>
+                <form action="insert.php" method="post">
+                    <div class="div_insert">
+                        <div>
+                            <label for="nombre_i">Nombre:</label>
+                            <input type="text" id='nombre_i' name='nombre_i'
+                                value=<?= $nombre_invalido ?>>
+                        </div>
+                        <div>
+                            <label for="color_i">Color:</label>
+                            <input type="color" id='color_i' name='color_i'>
+                        </div>
                     </div>
-                    <div>
-                        <label for="color_i">Color:</label>
-                        <input type="color" id='color_i' name='color_i' >
+                    <div class="error_insert">
+                        <?php if ($error_insert == "true") : ?>
+                            <p>Error en los datos</p>
+                        <?php endif; ?>
+                        <?php if ($nombre_existe) : ?>
+                            <p>El usuario ya existe</p>
+                        <?php endif; ?>
                     </div>
-                </div>
-                <div class="error_insert">
-                    <?php if($error_insert == "true") : ?>
-                    <p>Error en los datos</p>
-                    <?php endif; ?>
-                    <?php if($nombre_existe) : ?>
-                    <p>El usuario ya existe</p>
-                    <?php endif; ?>
-                </div>
 
-                <div class="buttons">
-                    <button type="submit">Enviar datos</button>
-                    <button type="reset">Borrar formulario</button>
-                </div>
-            </form>
-        </section>
+                    <div class="buttons">
+                        <button type="submit">Enviar datos</button>
+                        <button type="reset">Borrar formulario</button>
+                    </div>
+                </form>
+            </section>
+
+        <?php else : ?>
+            <section>
+                <h2>Actualiza los datos</h2>
+                <form action="update.php" method="post">
+                    <div class="div_update">
+                        <div>
+                            <label for="nombre_u">Nombre:</label>
+                            <input type="text" id='nombre_u' name='nombre_u'
+                                value="<?= $_GET['nombre'] ?>">
+                        </div>
+                        <div>
+                            <label for="color_u">Color:</label>
+                            <input type="color" id='color_u' name='color_u' 
+                                value="<?= '#'.$_GET['color'] ?>">
+
+                        </div>
+                    </div>
+                    <div class="error_insert">
+                        <?php if ($error_insert == "true") : ?>
+                            <p>Error en los datos</p>
+                        <?php endif; ?>
+                        <?php if ($nombre_existe) : ?>
+                            <p>El usuario ya existe</p>
+                        <?php endif; ?>
+                    </div>
+
+                    <div class="buttons">
+                        <button type="submit">Enviar datos</button>
+                        <button type="reset">Borrar formulario</button>
+                    </div>
+                </form>
+            </section>
+        <?php endif; ?>
 
     </main>
 </body>
+
 </html>
